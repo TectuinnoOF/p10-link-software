f016dc6053b7ac9fdf66af33f6c19da58cfb0093
pablo-gomez-perez <pablojesus34@hotmail.com>
2025-09-21 18:16:58 -0600

    first commit

---
+++ src/main/java/org/tectuinno/P10Soft/app/App.java
@@ -0,0 +1,41 @@
+/*
+ * This file is part of P10-Soft.
+ *
+ * P10-Soft is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful
+ * as a companion tool for the Tectuinno P10-Link chip, enabling users
+ * to design, visualize and transmit frames to P10 LED panels in real time.
+ * However, WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * As a special exception, you may use this file as part of a free software
+ * library without restriction. Specifically, if other files instantiate
+ * templates or use macros or inline functions from this file, or you compile
+ * this file and link it with other files to produce an executable, this
+ * file does not by itself cause the resulting executable to be covered by
+ * the GNU General Public License. This exception does not however
+ * invalidate any other reasons why the executable file might be covered by
+ * the GNU General Public License.
+ *
+ * Copyright 2025 Tectuinno Team (https://github.com/tectuinno)
+ */
+
+
+package org.tectuinno.P10Soft.app;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
